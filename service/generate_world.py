1658130517b17a4a4d5c322b6b9b42583548954c
chi zhang <chizhang@192.168.0.101>
2024-06-10 19:00:00 +0800

    [add] 提交替换框架

--- service/generate_world.py
+++ service/generate_world.py
@@ -1,5 +1,5 @@
 import os
-from docx import Document
+from docxtpl import DocxTemplate
 
 
 current_path = os.path.abspath(__file__)
@@ -22,9 +22,13 @@ class GenerateWorld(object):
 
     def start_generate_world(self, data) -> bool:
         # 根据文档位置加载数据并生成副本后保存文档
-        now_docx = Document(docx_template_path)
-        
-
-
+        now_docx = DocxTemplate(docx_template_path)
+        context = self.generate_context(data)
+        now_docx.render(context)
+        now_docx.save("")
         return True
+    
+    def generate_context(self, data) -> dict:
+        context = {}
         
+        return context
